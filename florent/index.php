d029e939a4cea30211106c2e038789496d4b4ecd
florent <florentartru@MacBook-Air-de-florent.local>
2015-11-04 16:29:44 +0100

    ajout des fichier de florent

---
+++ florent/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>inscription</title>
+</head>
+<body>
+	<form action="cible.php" method="POST">
+		<label for="lastname">Quel est votre nom ?</label><br>
+    	<input type="text" name="lastname" id="lastname" placeholder="Nom" /><br></p>
+
+    	<label for="firstname">Quel est votre prénom ?</label><br>
+    	<input type="text" name="firstname" id="firstname" placeholder="Prénom"/><p>
+
+    	<label for="age">Quel est votre âge ?</label><br>
+    	<input type="int" name="age" id="age" placeholder="âge" /><br></p>
+       
+       	<input type="submit"/>
+    </form>
+<br>
+<br>
+
+     	
+
+</body>
+</html>
